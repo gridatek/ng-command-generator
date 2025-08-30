d14de5401881e7de58fa4773fefa58d92ad34ef4
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-08-30 10:50:26 +0200

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home2',
-    loadComponent: () => import('./home/home2').then((m) => m.Home2),
-  },
-  {
     path: '',
     loadComponent: () => import('./home/home').then((m) => m.Home),
   },
